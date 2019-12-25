f89255360d600e29b122782c837355ab4d7c259f
Feihong Hsu <feihong.hsu@gmail.com>
2019-12-25 09:16:13 -0600

    Refactor randomHanzi to take int argument and return string

--- src/HelloWorld.purs
+++ src/HelloWorld.purs
@@ -2,20 +2,25 @@ module  HelloWorld where
 
 import Prelude
 
-import Effect (Effect, forE)
+import Effect (Effect)
 import Effect.Random (randomInt)
 import Effect.Console (log)
 import Data.Char (fromCharCode)
 import Data.Maybe (fromMaybe)
+import Data.Array ((..))
+import Data.Traversable (for)
+import Data.String.CodeUnits (fromCharArray)
 
-randomHanzi :: Effect Char
-randomHanzi = do
-  c <- fromCharCode <$> randomInt 0x4e00 0x9fff
-  pure $ fromMaybe '?' c
-
+randomHanzi :: Int -> Effect String
+randomHanzi n = do
+  fromCharArray <$> for (1..n) \_ -> randomChar
+  where
+    randomChar = do
+      c <- fromCharCode <$> randomInt 0x4e00 0x9fff
+      -- Will never return '?' in practice
+      pure $ fromMaybe '?' c
 
 main :: Effect Unit
 main = do
-  forE 1 10 $ \i -> do
-    c <- randomHanzi
-    log $ show c
+  s <- randomHanzi 8
+  log s
