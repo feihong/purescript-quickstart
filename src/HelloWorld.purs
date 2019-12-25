16f88f716cead1e1aa448654e6b4cc0d963d6725
Feihong Hsu <feihong.hsu@gmail.com>
2019-12-25 10:58:24 -0600

    Generate random code points instead of random chars

--- src/HelloWorld.purs
+++ src/HelloWorld.purs
@@ -9,18 +9,23 @@ import Data.Char (fromCharCode)
 import Data.Maybe (fromMaybe)
 import Data.Array ((..))
 import Data.Traversable (for)
-import Data.String.CodeUnits (fromCharArray)
+import Data.String.CodePoints (codePointFromChar, fromCodePointArray)
 
 randomHanzi :: Int -> Effect String
 randomHanzi n = do
-  fromCharArray <$> for (1..n) \_ -> randomChar
+  fromCodePointArray <$> for (1..n) \_ -> randomCodePoint
   where
-    randomChar = do
-      c <- fromCharCode <$> randomInt 0x4e00 0x9fff
+    randomCodePoint = do
       -- Will never return '?' in practice
-      pure $ fromMaybe '?' c
+      c <- (fromMaybe '?') <<< fromCharCode <$> randomInt 0x4e00 0x9fff
+      pure $ codePointFromChar c
 
 main :: Effect Unit
 main = do
   s <- randomHanzi 8
   log s
+
+  -- Alternate ways of using randomHanzi
+  randomHanzi 9 >>= \x -> log x
+  randomHanzi 10 >>= log
+  join $ log <$> randomHanzi 11
