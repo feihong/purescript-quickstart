a039935f28e758f0fc23eee5a46aa1abf31b2d14
Feihong Hsu <feihong.hsu@gmail.com>
2020-01-16 22:48:21 +0100

    Add set and over examples

--- src/HelloLens.purs
+++ src/HelloLens.purs
@@ -4,10 +4,18 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (logShow)
 import Data.Tuple (Tuple(..))
-import Data.Lens (view, _1, _2)
+import Data.Lens (view, set, over, _1, _2)
 
 main :: Effect Unit
 main = do
   logShow $ view _1 $ Tuple 6 "six"
 
   logShow $ view _2 $ Tuple 6 "six"
+
+  logShow $ set _1 7 $ Tuple 6 "six"
+
+  logShow $ set _2 "seven" $ Tuple 6 "six"
+
+  logShow $ over _1 (_ + 1) $ Tuple 6 "six"
+
+  logShow $ over _2 (_ <> " cakes") $ Tuple 6 "six"
