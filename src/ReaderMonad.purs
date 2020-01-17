c0c9b971937064cd177c3fafd7e72493803cca74
Feihong Hsu <feihong.hsu@gmail.com>
2020-01-17 18:54:00 +0100

    Add trivial Reader monad example

--- src/ReaderMonad.purs
+++ src/ReaderMonad.purs
@@ -1,11 +1,28 @@
--- http://adit.io/posts/2013-06-10-three-useful-monads.html
 module ReaderMonad where
 
 import Prelude
 import Effect (Effect)
-import Effect.Console (log)
--- import Control.Monad.Reader (Reader, runReader, ask)
+import Effect.Console (logShow)
+import Control.Monad.Reader (Reader, runReader, ask, asks)
+
+type Env =
+  { foo :: String
+  , bar :: Int
+  }
+
+doStuff :: Reader Env String
+doStuff = do
+  env <- ask
+  pure $ "Do stuff with foo=" <> env.foo <> " and bar=" <> show env.bar
+
+doStuff2 :: Reader Env String
+doStuff2 = do
+  foo <- asks \e -> e.foo
+  bar <- asks \e -> e.bar
+  pure $ "Do stuff with foo=" <> foo <> " and bar=" <> show bar
 
 main :: Effect Unit
 main = do
-  log "woo"
+  logShow $ runReader doStuff { foo: "Fooey", bar: 88 }
+
+  logShow $ runReader doStuff2 { foo: "Fooey", bar: 88 }
