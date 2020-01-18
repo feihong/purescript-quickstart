ab93bd4c89f0f4023c6e234bd153d2035e89f368
Feihong Hsu <feihong.hsu@gmail.com>
2020-01-18 18:54:16 +0100

    Implement list files using WriterT

--- src/WriterTListFiles.purs
+++ src/WriterTListFiles.purs
@@ -5,6 +5,7 @@ import Effect (Effect)
 import Effect.Console (logShow)
 import Effect.Class (liftEffect)
 import Effect.Aff (Aff, launchAff_)
+import Effect.Aff.Class (liftAff)
 import Data.Int (fromNumber)
 import Data.Tuple (Tuple(..))
 import Data.Maybe (fromMaybe)
@@ -13,12 +14,14 @@ import Data.Array ((!!))
 import Node.FS.Aff (readdir, stat)
 import Node.FS.Stats (Stats(..), isDirectory)
 import Node.Process (argv)
+import Control.Monad.Writer (WriterT, tell, execWriterT)
 
 listFiles :: String -> Aff (Array (Tuple String Int))
 listFiles root = do
   files <- readdir root
   join <$> sequence (getPair root <$> files)
   where
+    -- todo: exclude files that start with .
     getPair :: String -> String -> Aff (Array (Tuple String Int))
     getPair path child = do
       let path' = path <> "/" <> child
@@ -29,9 +32,25 @@ listFiles root = do
       else
         pure $ [Tuple path' (fromMaybe 0 $ fromNumber size)]
 
+listFilesUsingWriterT :: String -> WriterT (Array (Tuple String Int)) Aff Unit
+listFilesUsingWriterT root = do
+  files <- liftAff $ readdir root
+  for_ files \file -> do
+    let path = root <> "/" <> file
+    stats <- liftAff $ stat path
+    let Stats { size } = stats
+    if isDirectory stats then
+      listFilesUsingWriterT path
+    else
+      tell $ [Tuple path (fromMaybe 0 $ fromNumber size)]
+
 main :: Effect Unit
 main = do
   root <- (\args -> fromMaybe "." $ args !! 2) <$> argv
   launchAff_ do
-    files <- listFiles root
+    -- files <- listFiles root
+    -- for_ files (liftEffect <<< logShow)
+
+    let writer = listFilesUsingWriterT root
+    files <- execWriterT writer
     for_ files (liftEffect <<< logShow)
